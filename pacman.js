ffd176a088b59e3af35211d0e9961ac2c4560de6
Chazie9 <charlesharring3@gmail.com>
2018-08-19 12:46:47 -0700

    downloaded starter file

---
+++ pacman.js
@@ -0,0 +1,21 @@
+/**
+ * Write a file docstring here
+ * 
+ * Author: Chas Harring
+ */
+
+
+/**
+ * Use the function below to format your input/output arguments. Be sure not to change the order of the elements in the output array. 
+ * Remember that code organization is very important to us, so we encourage the use of helper functions/separate files as you see fit.
+ * Input:
+ *      1. inputFile (String) = contains the name of a text file you need to read that is in the same directory, includes the ".txt" extension
+ *         (ie. "input.txt")
+ * Output:
+ *      1. array containing the final x position of Pacman, final y position of Pacman, and total number of 
+ *         coins collected in that order (ie. [finalXPos, finalYPos, coinsCollected])
+ */
+function pacman(inputFile) {
+    // Start writing your code here
+	return [finalXPos, finalYPos, coinsCollected];
+}
