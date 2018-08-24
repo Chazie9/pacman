63e117c8b0e539856198f7b86d2f49555cc84674
Chazie9 <charlesharring3@gmail.com>
2018-08-23 23:36:02 -0700

    check more edge cases and accept massive boards

--- helperFunctions/validateData.js
+++ helperFunctions/validateData.js
@@ -1,10 +1,27 @@
 function hasExtraSpaces(data) {
-    console.log('what is data anyway?', data)
-    if (data.includes('')) {
-        console.log('Please make sure inputs are in seperate files or remove spaces after the line in .txt file');
-        return true;
-      }
-      return false;
+  let index = data.length - 1;
+  while (index >= 0) {
+    if (data[index] === '' || data[index] === ' ') {
+      data.splice(index, 1);
+    }
+    index -= 1;
+  }
+
+  if (data.includes('')) {
+    console.log('Please make sure inputs are in seperate files or remove spaces after the line in .txt file');
+    return true;
+  }
+  return false;
 }
 
+
+function validBoardPosition(board, pos) {
+  if( pos.xPos > board.xMax || pos.xPos < board.xMin || pos.yPos > board.yMax || pos.yPos < board.yMin) {
+    console.log('here 3A' )
+    return true;
+  }
+}
+
+
+module.exports.validBoardPosition = validBoardPosition;
 module.exports.hasExtraSpaces = hasExtraSpaces;
